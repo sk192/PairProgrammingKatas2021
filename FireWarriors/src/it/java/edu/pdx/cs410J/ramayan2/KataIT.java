846b9f62e698a5cec175605a313d771e84bcc63b
HarshaRamayanam <63016598+HarshaRamayanam@users.noreply.github.com>
2021-07-14 19:56:04 -0700

    Updated the files for the first time.

--- FireWarriors/src/it/java/edu/pdx/cs410J/ramayan2/KataIT.java
+++ FireWarriors/src/it/java/edu/pdx/cs410J/ramayan2/KataIT.java
@@ -1,5 +1,6 @@
 package edu.pdx.cs410J.ramayan2;
 
+
 import edu.pdx.cs410J.InvokeMainTestCase;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,7 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.StringContains.containsString;
 
 class KataIT extends InvokeMainTestCase {
+
 
   @Test
   void invokingMainWithNoArgumentsHasExitCodeOf1() {
@@ -21,5 +23,53 @@ class KataIT extends InvokeMainTestCase {
     assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
   }
 
+  @Test
+  void divByThreeShouldWriteFoo() {
+    InvokeMainTestCase.MainMethodResult resultFor3 = invokeMain(Kata.class, "3");
+    InvokeMainTestCase.MainMethodResult resultFor6 = invokeMain(Kata.class, "6");
+    InvokeMainTestCase.MainMethodResult resultFor9 = invokeMain(Kata.class, "6");
+
+    assertThat(resultFor3.getTextWrittenToStandardOut(), containsString("Foo"));
+    assertThat(resultFor6.getTextWrittenToStandardOut(), containsString("Foo"));
+    assertThat(resultFor9.getTextWrittenToStandardOut(), containsString("Foo"));
+  }
+
+  @Test
+  void divByFiveShouldContainsBar() {
+    InvokeMainTestCase.MainMethodResult resultFor5 = invokeMain(Kata.class, "5");
+    InvokeMainTestCase.MainMethodResult resultFor10 = invokeMain(Kata.class, "10");
+//    InvokeMainTestCase.MainMethodResult resultFor15 = invokeMain(Kata.class, "15");
+
+    assertThat(resultFor5.getTextWrittenToStandardOut(), containsString("Bar"));
+    assertThat(resultFor10.getTextWrittenToStandardOut(), containsString("Bar"));
+//    assertThat(resultFor15.getTextWrittenToStandardOut(), containsString("Bar"));
+  }
+
+  @Test
+  void divByFiveSAndThreeShouldContainsFooBar() {
+    InvokeMainTestCase.MainMethodResult resultFor30 = invokeMain(Kata.class, "30");
+    InvokeMainTestCase.MainMethodResult resultFor15 = invokeMain(Kata.class, "15");
+
+    assertThat(resultFor30.getTextWrittenToStandardOut(), containsString("FooBar"));
+    assertThat(resultFor15.getTextWrittenToStandardOut(), containsString("FooBar"));
+  }
+
+  @Test
+  void divBySevenShouldContainsFooBar() {
+    InvokeMainTestCase.MainMethodResult resultFor30 = invokeMain(Kata.class, "7");
+    InvokeMainTestCase.MainMethodResult resultFor15 = invokeMain(Kata.class, "14");
+
+    assertThat(resultFor30.getTextWrittenToStandardOut(), containsString("Qix"));
+    assertThat(resultFor15.getTextWrittenToStandardOut(), containsString("Qix"));
+  }
+
+  @Test
+  void numberContainingThreeFiveSevenShouldContainFooBarQixInTheOrder() {
+    InvokeMainTestCase.MainMethodResult resultFor357 = invokeMain(Kata.class, "357");
+    InvokeMainTestCase.MainMethodResult resultFor573 = invokeMain(Kata.class, "573");
+
+    assertThat(resultFor357.getTextWrittenToStandardOut(), containsString("FooBarQix"));
+    assertThat(resultFor573.getTextWrittenToStandardOut(), containsString("BarQixFoo"));
+  }
 
 }
--- FireWarriors/src/main/java/edu/pdx/cs410J/ramayan2/Kata.java
+++ FireWarriors/src/main/java/edu/pdx/cs410J/ramayan2/Kata.java
@@ -1,5 +1,7 @@
 package edu.pdx.cs410J.ramayan2;
 
+import java.util.Arrays;
+
 /**
  * A class for getting started with a code kata
  *
@@ -7,11 +9,76 @@ package edu.pdx.cs410J.ramayan2;
  * class (and its tests).
  */
 public class Kata {
-                                                                                    
+
+
 
   public static void main(String[] args) {
+
     System.err.println("Missing command line arguments");
-    System.out.println("Harsha");
+
+    StringBuilder ans = getStringBuilder(args[0]);
+
+    System.out.println(ans);
+
+
+
+
+
+
+//    StringBuilder ans = new StringBuilder();
+//    for (int i = 0; i < args[0].length(); i++) {
+//      System.out.println(args[0]);
+//
+////      int number = Integer.parseInt(args[0][i]);
+//
+////      if (number == 3) {
+////        ans.append("Foo");
+////      } else if (number == 5) {
+////        ans.append("Bar");
+////      } else if (number == 7) {
+////        ans.append("Qix");
+////      }
+//    }
+
+    System.exit(0);
+
+
+
+
+
+
+
+
+
+    int num = Integer.parseInt(args[0]);
+
+    if(num % 3 == 0 && num % 5 == 0){
+      System.out.println("FooBar");
+    } else if (num % 3 == 0) {
+      System.out.println("Foo");
+    } else if (num % 5 == 0) {
+      System.out.println("Bar");
+    } else if(num % 7 == 0){
+      System.out.println("Qix");
+    }
     System.exit(1);
   }
+
+  private static StringBuilder getStringBuilder(String arg) {
+    char[] digits = arg.toCharArray();
+//    System.out.println("digits: " + Arrays.toString(digits));
+    StringBuilder ans = new StringBuilder();
+
+    for (char digit : digits) {
+      if (digit == '3') {
+        ans.append("Foo");
+      } else  if (digit == '5') {
+        ans.append("Bar");
+      } else if (digit == '7') {
+        ans.append("Qix");
+      }
+    }
+    return ans;
+  }
+
 }
